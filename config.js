ff89e2c3ab3dcac59bb30f4e4f15f4e6283f55db
NavinduNimsara2 <148603247+NavinduNimsara2@users.noreply.github.com>
2025-02-08 07:32:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || 'SEON-MD=KIhQjY5Z#eDpHOVIXsyjHVbbdZsYWGyMJ54jd0mKyg4hB6yeOgng', //Put Session-id Here
 GITHUB_USERNAME: process.env.GITHUB_USERNAME || 'NavinduNimsara2', //Put Your Gitub Username
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || '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', //Put Your Github Auth Token
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || 'ghp_pSUr10kRROuRZJmX9ksKuyFfbxYStN2aovjp', //Put Your Github Auth Token
 REPO_NAME: process.env.REPO_NAME || 'SEON-MD-DATABASE' //Bot Database Repo Name
 };
